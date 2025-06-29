3a262563ed0d034267bb4f26e5e4e7b6ff444d9c
Your Name <kdhakad602@gmail.com>
2025-06-30 01:44:00 +0530

    How to create an ArrayList

---
+++ src/_029_ArrayList/_01_introduction.java
@@ -0,0 +1,14 @@
+package _029_ArrayList;
+import java.util.ArrayList;
+//How to create an ArrayList
+
+public class _01_introduction {
+    public static void main(String[] args) {
+        //String | Float | Boolean | Integer --> types of data, we store in the angular brackets
+      //ClassName objectName = new ClassName();
+        ArrayList<Integer> list = new ArrayList<>(); //list is the object name, and Integer is the type of data we want to store in the ArrayList
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+        ArrayList<Float> list4 = new ArrayList<>();
+    }
+}
