2aabc714fce03ae1b2a06be49d4e002141498bea
Your Name <kdhakad602@gmail.com>
2025-06-30 01:44:55 +0530

    Find the Maximum Element in ArrayList

---
+++ src/_029_ArrayList/_05_Find_Maximum.java
@@ -0,0 +1,30 @@
+package _029_ArrayList;
+import java.util.ArrayList;
+//Find the Maximum Element in ArrayList
+//Time Complexity = O(n)
+
+public class _05_Find_Maximum {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+         int max = Integer.MIN_VALUE;
+         for(int i=0; i<list.size(); i++){
+//            if(max < list.get(i)){
+//                 max = list.get(i);
+//             }
+             max = Math.max(max, list.get(i));
+         }
+
+        System.out.println("Maximum Element is = "+max);
+    }
+}
+
+/*
+Output:
+Maximum Element is = 9
+ */
