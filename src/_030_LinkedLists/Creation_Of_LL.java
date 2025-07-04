9a1389a8633d8967bd8dcf62d08d87d5b4ced16d
Your Name <kdhakad602@gmail.com>
2025-07-04 20:03:45 +0530

    creation of single node in the linked list

---
+++ src/_030_LinkedLists/Creation_Of_LL.java
@@ -0,0 +1,14 @@
+package _030_LinkedLists;
+//creation of single node in the linked list
+
+public class Creation_Of_LL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
