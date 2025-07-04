4545b43845a2870461944c07e30d86af9c1f3b99
Your Name <kdhakad602@gmail.com>
2025-07-04 20:03:29 +0530

    Java Collection Framework

---
+++ src/_030_LinkedLists/Classroom.java
@@ -0,0 +1,27 @@
+package _030_LinkedLists;
+//Java Collection Framework
+import java.util.LinkedList;
+
+public class Classroom {
+    public static void main(String[] args) {
+        //create - objects int,float,boolean -> Integer,Float,Character
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        //0->1->2
+        System.out.println(ll);
+
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
+/*
+Output:
+[0, 1, 2]
+[1]
+ */
