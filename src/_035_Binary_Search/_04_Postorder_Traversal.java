196902c6a117e923ffc952f53999f876534969f7
Your Name <kdhakad602@gmail.com>
2025-08-09 16:10:17 +0530

    Postorder Traversal

---
+++ src/_035_Binary_Search/_04_Postorder_Traversal.java
@@ -0,0 +1,52 @@
+package _035_Binary_Search;
+//Postorder Traversal
+//Time complexity: o(n)
+
+public class _04_Postorder_Traversal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+        static Node buildTree(int[] nodes) {
+            idx++;
+            if(nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        public static void postorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        tree.postorder(root);
+    }
+}
+/*
+Output:
+4 5 2 6 3 1
+ */
